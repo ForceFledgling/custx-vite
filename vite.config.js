d6f38be9de152d0fcb0062f64c9b03e1a92ee815
Владимир Пензин <vladimir@MacBook-Pro-Vladimir.local>
2023-01-06 16:21:18 +0500

    :rotating_light: Testing cors

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://api.custx.ru',
-        changeOrigin: true,
+        changeOrigin: false,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
